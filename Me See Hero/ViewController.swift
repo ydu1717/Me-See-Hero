be7c2842c5e92f5610317a5bda16a6d77892b9a8
ydu1717 <ydu17@u.rochester.edu>
2019-10-31 21:16:06 +0800

    Initial Commit

---
+++ Me See Hero/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Me See Hero
+//
+//  Created by mac on 2019/10/31.
+//  Copyright © 2019 ydu1717. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
